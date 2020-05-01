21800c8428946c5cdebfb959ae20c25b71ada3a4
CurtisNewbie <zhuangyongj@gmail.com>
2020-05-01 13:39:15 +0800

    Implement RestClient for fetch repo data from GitHub

---
+++ rest-backend/src/main/java/com/curtisnewbie/restclient/GithubClient.java
@@ -0,0 +1,41 @@
+package com.curtisnewbie.restclient;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+/**
+ * ------------------------------------
+ * <p>
+ * Author: Yongjie Zhuang
+ * <p>
+ * ------------------------------------
+ * <p>
+ * RestClient for fetching repositories data from github. The base url is
+ * defined in {@code config.repo.client} in properties. Only public accessible
+ * repositories can be fetched, since this client does not use any
+ * authentication mechanism.
+ * </p>
+ */
+@RegisterRestClient(configKey = "config.repo.client")
+public interface GithubClient {
+
+    @GET
+    @Path("/users/{username}/repos")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response fetchAllRepos(@PathParam("username") String username);
+
+    @GET
+    @Path("/repos/{username}/{repo}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response fetchRepo(@PathParam("username") String username, @PathParam("repo") String repoName);
+
+}
