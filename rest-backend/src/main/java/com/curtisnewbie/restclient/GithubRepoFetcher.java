37bc8bf25cf2b4d579b0b85ef5f71345a2b922df
CurtisNewbie <zhuangyongj@gmail.com>
2020-05-01 13:50:19 +0800

    Impl GithubRepoFetcher
    This class creates a new Thread that uses GithubClient (RestClient)
    to fetches repositories data from Github in every N Minutes.

---
+++ rest-backend/src/main/java/com/curtisnewbie/restclient/GithubRepoFetcher.java
@@ -0,0 +1,91 @@
+package com.curtisnewbie.restclient;
+
+import java.util.List;
+
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.logging.Logger;
+
+import io.quarkus.runtime.StartupEvent;
+
+/**
+ * ------------------------------------
+ * 
+ * Author: Yongjie Zhuang
+ * 
+ * ------------------------------------
+ * <p>
+ * This class is responsible for fetching data of repositories from Github.
+ * Which repositories to fetch are dependent on configuration in
+ * {@code config.repo.names}, and how frequent (in minutes) should the
+ * repositories be fetched is defined in {@code config.repo.freq.min}
+ * </p>
+ */
+public class GithubRepoFetcher {
+    private final int MIN = 60 * 1000;
+    private final Logger logger = Logger.getLogger(this.getClass());
+
+    @ConfigProperty(name = "config.repo.names")
+    protected List<String> repoNames;
+
+    @ConfigProperty(name = "config.repo.freq.min")
+    protected int freqInMin;
+
+    @Inject
+    @RestClient
+    protected GithubClient client;
+
+    /**
+     * Start a new thread on app startup to fetch and update repositories
+     * repeatively in every N minutes.
+     * 
+     * @param ev
+     * @see {@link GithubRepoFetcher#freqInMin}
+     */
+    void onStart(@Observes StartupEvent ev) {
+        logConfig();
+        new Thread(() -> {
+            logger.info(String.format("Initialising %s", this.getClass().getName()));
+            if (repoNames.size() == 0 && repoNames.get(0).trim().equals("*")) {
+                fetchAll();
+            } else {
+                for (String repo : repoNames)
+                    fetch(repo);
+            }
+            try {
+                Thread.sleep(freqInMin * MIN);
+            } catch (InterruptedException e) {
+                logger.error(e);
+            }
+        }).start();
+    }
+
+    /**
+     * Fetch one repository
+     * 
+     * @param repoName
+     */
+    void fetch(String repoName) {
+        // TODO: finish implentation
+        logger.info(client.fetchRepo("curtisnewbie", repoName).readEntity(String.class));
+    }
+
+    /**
+     * Fetch all repositories that are accessible
+     */
+    void fetchAll() {
+        // TODO: finish implentation
+        logger.info(client.fetchAllRepos("curtisnewbie").readEntity(String.class));
+    }
+
+    /**
+     * Log config used
+     */
+    private void logConfig() {
+        logger.info(String.format("%s using config: { config.repo.names : %s, config.repo.freq.min: %d }",
+                this.getClass().getName(), this.repoNames.toString(), this.freqInMin));
+    }
+}
