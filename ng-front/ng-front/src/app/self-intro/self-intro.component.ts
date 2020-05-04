f202f5927900740c053aba485537dd68d597a6d2
CurtisNewbie <zhuangyongj@gmail.com>
2020-05-04 18:17:23 +0800

    Update intro typing animation

--- ng-front/ng-front/src/app/self-intro/self-intro.component.ts
+++ ng-front/ng-front/src/app/self-intro/self-intro.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, AfterViewInit } from "@angular/core";
 import { HttpService } from "../http.service";
 
+const ANIMATION_CHAR_INTERVAL = 50;
+const ANIMATION_INTERVAL = 5000;
+
 @Component({
   selector: "app-self-intro",
   templateUrl: "./self-intro.component.html",
@@ -8,6 +11,7 @@ import { HttpService } from "../http.service";
 })
 export class SelfIntroComponent implements OnInit, AfterViewInit {
   githubUrl: string = this.http.getGithubUrl();
+  introTxt = this.http.getIntroduction();
   intro: string[] = [];
 
   constructor(private http: HttpService) {}
@@ -15,33 +19,41 @@ export class SelfIntroComponent implements OnInit, AfterViewInit {
   ngOnInit() {}
 
   ngAfterViewInit() {
-    let introTxt = this.http.getIntroduction();
-    this.introTypingAnim(introTxt);
+    this.introTypingAnim(ANIMATION_INTERVAL, ANIMATION_CHAR_INTERVAL);
   }
 
   /**
-   * Start typing animation for introduction
+   * Start typing animation for introduction. When current animation finishes, it will wait for a moment
+   * and repeat again.
+   *
+   * @param animInterval interval for the whole animation
+   * @param charInterval interval for each character (i.e., how fast each character is typed)
    */
-  private introTypingAnim(txt: string[]): void {
-    if (txt.length > 0) {
-      const INTERVAL = 55;
+  private introTypingAnim(animInterval: number, charInterval: number): void {
+    if (this.introTxt.length > 0) {
       const typingHandler = () => {
-        if (charIndex < txt[paraIndex].length) {
-          this.intro[paraIndex] += txt[paraIndex].charAt(charIndex++);
+        if (charIndex < this.introTxt[paraIndex].length) {
+          this.intro[paraIndex] += this.introTxt[paraIndex].charAt(charIndex++);
         }
-        if (charIndex < txt[paraIndex].length) {
-          setTimeout(typingHandler, INTERVAL);
+        if (charIndex < this.introTxt[paraIndex].length) {
+          setTimeout(typingHandler, charInterval);
         } else if (paraIndex < numOfPara - 1) {
           this.intro[++paraIndex] = "";
           charIndex = 0;
-          setTimeout(typingHandler, INTERVAL);
+          setTimeout(typingHandler, charInterval);
+        } else {
+          // reset animation, and do it again
+          setTimeout(() => {
+            this.intro = [];
+            this.introTypingAnim(animInterval, charInterval);
+          }, 5000);
         }
       };
-      let numOfPara = txt.length;
+      let numOfPara = this.introTxt.length;
       let paraIndex = 0;
       let charIndex = 0;
       this.intro[paraIndex] = "";
-      setTimeout(typingHandler, INTERVAL);
+      setTimeout(typingHandler, charInterval);
     }
   }
 }
